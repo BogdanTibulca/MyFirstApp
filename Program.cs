5e0cd85e7bdbfc13edee8b7491a35711701963ce
BogdanTibulca <bogdantibulca@yahoo.com>
2021-10-16 11:32:51 +0300

    First commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MyFirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First commit");
         }
     }
 }
